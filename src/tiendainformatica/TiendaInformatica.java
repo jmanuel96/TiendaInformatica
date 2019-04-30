190eeb6eef7522e55eb48b816a10d797a3da101e
josemanuel <josemanuel@JoseManuel>
2019-04-30 10:22:55 +0200

    30 abril 2019

--- src/tiendainformatica/TiendaInformatica.java
+++ src/tiendainformatica/TiendaInformatica.java
@@ -5,13 +5,16 @@
  */
 package tiendainformatica;
 
+import BaseDatos.Categorias;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 /**
  *
@@ -28,7 +31,48 @@ public class TiendaInformatica {
         emfProperties.put("javax.persistence.schema-generation.database.action", "create");
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("TiendaInformaticaPU", emfProperties); // Comprobar nombre de la PU
         EntityManager em = emf.createEntityManager();
+        Query queryCategorias = em.createNamedQuery("Categorias.findAll");
+        List<Categorias> listCategorias = queryCategorias.getResultList();
+        
+        for(Categorias categoria : listCategorias) {
+            System.out.println(categoria.getNombre());
+        }
+        
+        for(int i=0; i<listCategorias.size(); i++) {
+            Categorias categoria = listCategorias.get(i);
+            System.out.println(categoria.getNombre());
+}
+
+//            Query queryProvinciaCadiz = em.createNamedQuery("Categorias.findByNombre");
+//            queryProvinciaCadiz.setParameter("nombre", "Cádiz");
+//            List<Categorias> listProvinciasCadiz = queryProvinciaCadiz.getResultList();
+//            for(Categorias provinciaCadiz : listProvinciasCadiz) {
+//                System.out.print(provinciaCadiz.getId() + ": ");
+//                System.out.println(provinciaCadiz.getNombre());
+//}
+        
+        // Iniciar y finalizar transaciones
+        em.getTransaction().begin();
+        
+        // Añadir aquí las operaciones de modificación de la base de datos
+        Categorias ordenadores = new Categorias();
+        ordenadores.setNombre("Ordenadores");
+        Categorias moviles = new Categorias();
+        moviles.setNombre("Móviles");
+        Categorias consolas = new Categorias();
+        consolas.setNombre("Consolas");
+        Categorias televisores = new Categorias();
+        televisores.setNombre("Televisores");
+        em.persist(ordenadores);
+        em.persist(moviles);
+        em.persist(consolas);
+        em.persist(televisores);
+        
+        
+        
+        em.getTransaction().commit();
     
+        // Cerrar la conexión con la base de datos
         em.close(); 
         emf.close(); 
         try { 
@@ -36,6 +80,5 @@ public class TiendaInformatica {
         } catch (SQLException ex) { 
         }
     }
-
-    
+   
 }
